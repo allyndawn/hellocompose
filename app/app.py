22a90c231bdc3a7e4e5f403c1d094b3b7e689726
allendavidsnook <allendavidsnook@gmail.com>
2022-01-14 15:28:42 -0800

    Add ElasticMQ connection to app and microservice

--- app/app.py
+++ app/app.py
@@ -1,2 +1,14 @@
+import boto3
+
+# Note: dummy values for aws_secret_access_key and aws_access_key_id are needed
+# to avoid botocore.exceptions.NoCredentialsError: Unable to locate credentials
+client = boto3.resource('sqs',
+                        endpoint_url='http://elasticmq:9324',
+                        region_name='elasticmq',
+                        aws_secret_access_key='x',
+                        aws_access_key_id='x',
+                        use_ssl=False)
+queue = client.get_queue_by_name(QueueName='devicemessage')
+
 if __name__ == "__main__":
     print("started app.py")
